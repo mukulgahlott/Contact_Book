8397afeb3dcaff81edd8e7171d5fbc8ccfc5759a
mukul gahlot <161557361+mukulgahlott@users.noreply.github.com>
2024-06-16 09:59:07 +0530

    Update Utility.java

--- src/main/java/org/coretechies/util/Utility.java
+++ src/main/java/org/coretechies/util/Utility.java
@@ -1,42 +1,35 @@
 package org.coretechies.util;
 
 //important packages that should be import
-import org.coretechies.model.Contact;
-import org.coretechies.ui.ContactBookScreen;
-
-import javax.swing.table.DefaultTableModel;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
-import static org.coretechies.model.UpdateContacts.*;
-import static org.coretechies.ui.ContactBookScreen.*;
+import javax.swing.table.DefaultTableModel;
+
+import org.coretechies.model.Contact;
+import static org.coretechies.model.UpdateContacts.contact1;
+import static org.coretechies.ui.ContactBookScreen.contactTable;
 
 public class Utility {
 
     // sort the table contacts
     public static void sort(List<Contact> contacts1) {
         contacts1.sort(Comparator.comparing(Contact::getName));
-
     }
 
     // search the Contacts and update the table
-    public static void search() {
-        String[] header = {"NAME", "NUMBER"};
-        tableModel = new DefaultTableModel(header, 0);
-        DefaultTableModel searchTableModal = new DefaultTableModel();
+    public static void search(String key) {
+        String[] header = { "NAME", "NUMBER" };
+        DefaultTableModel searchTableModel = new DefaultTableModel(header, 0);
         for (Contact c : contact1) {
-            if (c.getName().contains(ContactBookScreen.key)) {
-                Object[] rowData = {c.getName(), c.getNumber()};
-                searchTableModal.addRow(rowData);
-                System.out.println(Arrays.toString(rowData));
+            if (c.getName().contains(key)) {
+                Object[] rowData = { c.getName(), c.getNumber() };
+                searchTableModel.addRow(rowData);
             }
         }
-        if (searchTableModal.getRowCount() == 0) {
+        if (searchTableModel.getRowCount() == 0) {
             System.out.println("Contact not found");
-        } else {
-            contactTable.setModel(searchTableModal);
         }
-
+        contactTable.setModel(searchTableModel);
     }
 }
